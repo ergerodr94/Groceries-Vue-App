63cb5fcb6a2205bf749b48d22d52f3cb68813bad
Erick Rodriguez <ergerodr@gmail.com>
2023-09-30 20:34:35 -0700

    Added Vuetify 3

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 FROM node:20-alpine
 
-COPY package*.json /app/
-
 # make the 'app' folder the current working directory
 WORKDIR /app/
 
-# install project dependencies
-RUN npm install
+COPY package*.json /app/
 
+# install project dependencies
+RUN npm install 
+
+# copy project files and folders to the current working directory (i.e. 'app' folder)
+COPY . .
+
+# copy node_modules to the container
+#COPY --from=0 /app/node_modules /app/node_modules
 
 EXPOSE 8080
 CMD ["npm", "run", "serve"]
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RecipeGrid from '../components/RecipeGrid'
+import RecipeDetails from '../components/RecipeDetails'
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/recipe/:id',
+    name: 'recipeDetails',
+    component: RecipeDetails
   }
 ]
 
