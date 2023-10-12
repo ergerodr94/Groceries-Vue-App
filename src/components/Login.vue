0a018ad79eb8126d0efa8dface88dc7f9f244b89
Erick Rodriguez <ergerodr@ucsc.edu>
2023-10-11 23:52:52 -0700

    Added Firebase as a Service

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,9 +12,14 @@
           :append-icon="show1 ? 'mdi-eye-off': 'mdi-eye'"
           :type="show1 ? 'text' : 'password'"
           label="Password"
+          @click:append="show1 = !show1"
         ></v-text-field>
   
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
+        <v-btn 
+          type="log-in" 
+          block 
+          class="mt-2"
+          @click="handleLogin">Log in</v-btn>
         <v-btn type="sign-up" block class="mt-3"> Sign Up</v-btn>
         <v-btn type="lost-password" block class="mt-3"> Lost Password</v-btn>
       </v-form>
@@ -22,17 +27,37 @@
   </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'LoginComp',
     data() {
         return {
             show1: false,
             email: "",
-            password: ""
+            password: "",
+            loginResponse: ""
         }
     },
     methods: {
-        // Your methods here
+      handleLogin(){
+        // Replace with your actual API endpoint
+        const apiUrl = `http://localhost:8000/groceriesapi/groceriesapi/loginVue`;
+        var body = {
+          email: this.email,
+          password: this.password
+        }
+
+        // Use Axios or a similar library to make the GET request
+        axios.post(apiUrl, body)
+          .then(response => {
+            this.loginResponse = response.data;
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error('Error during authentication:', error);
+          });
+      }
     },
     computed: {
         // Your computed properties here
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -44,6 +49,21 @@ const vuetifyApp = createVuetify({
 
 const app = createApp(App).use(store).use(router).use(vuetifyApp).mount('#app')
 
+const firebaseConfig = { 
+    apiKey: "AIzaSyC6_5UcHWmpAV5EZPUXbnJrTcRL7KTrNuU",
+    authDomain: "unpack-the-pantry-fc442.firebaseapp.com",
+    projectId: "unpack-the-pantry-fc442",
+    storageBucket: "unpack-the-pantry-fc442.appspot.com",
+    messagingSenderId: "359464108209",
+    appId: "1:359464108209:web:6514126bca875ebeebe290",
+    measurementId: "G-78G6T9CCV3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
 
     
 
