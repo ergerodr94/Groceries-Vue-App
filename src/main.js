89dbaf0a5d23f643ef2b33bf941eb902af0f64da
Erick Rodriguez <ergerodr@ucsc.edu>
2023-10-23 17:08:36 -0700

    Connected app to Firebase Emulators

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { VueElement, createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import 'firebase/compat/storage';
+
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -47,24 +44,6 @@ const vuetifyApp = createVuetify({
 })
 
 const app = createApp(App).use(store).use(router).use(vuetifyApp).mount('#app')
-
-const firebaseConfig = { 
-    apiKey: "AIzaSyC6_5UcHWmpAV5EZPUXbnJrTcRL7KTrNuU",
-    authDomain: "unpack-the-pantry-fc442.firebaseapp.com",
-    projectId: "unpack-the-pantry-fc442",
-    storageBucket: "unpack-the-pantry-fc442.appspot.com",
-    messagingSenderId: "359464108209",
-    appId: "1:359464108209:web:6514126bca875ebeebe290",
-    measurementId: "G-78G6T9CCV3"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
-export const auth = firebase.auth();
-export const storage = firebase.storage();
-
-console.log(firebase);    
 
 
 
