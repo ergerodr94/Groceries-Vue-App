b28c6111e6605e3f0557f9265d75e7a61d8fbdc4
Erick Rodriguez <ergerodr@gmail.com>
2024-01-02 13:43:07 -0800

    Moved Login Functionality to a very basic vuex store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { VueElement, createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 
 
 import 'vuetify/styles'
---
+++ src/store/store.js
@@ -0,0 +1,66 @@
+import { createStore } from 'vuex'
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { auth } from '../firebase.js'
+import axios from 'axios';
+import firebase from 'firebase/compat/app';
+
+onAuthStateChanged(auth, (user) => {
+  if(user){
+  // https://firebase.google.com/docs/reference/js/auth.user
+  const uid = user.uid;
+  console.log("uid: ", uid)
+  } else { 
+    // User is signed out
+  }
+})
+
+export default createStore({
+
+  state: {
+    //Where our Data is stored.  
+    show1: false,
+    email: "",
+    user: "", 
+    password: "",
+    loginResponse: ""
+  },
+
+  getters: {
+    //allows us to get data from our state. 
+  },
+
+  mutations: {
+    //Methods that change the data in the state. Mutations are committed(triggered). 
+    handleLogin(state){
+      // Replace with your actual API endpoint
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          // Get Google Access Token
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken; 
+          console.log("Token: ", token)
+          //Signed in user info
+          state.user = result.user;
+        }).catch((error) => {
+          const errorCode = error.code;
+          console.log(errorCode);
+          const errorMessage = error.message;
+          console.log(errorMessage);
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          console.log(credential);
+        });
+    },
+    handleLogout(state){
+      state.user = "";
+    }
+    
+  },
+
+  actions: {
+    //Methods that can't change data in the state, but they can call mutations. Actions are dispached(triggered).
+  },
+
+  modules: {
+  }
+})
