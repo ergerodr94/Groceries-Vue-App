524537549528a8abc1d848d16ae32d6b1dd51de9
Erick Rodriguez <ergerodr@ucsc.edu>
2024-05-29 23:22:23 -0700

    form validation for item name is complete

--- src/components/UploadGroceries.vue
+++ src/components/UploadGroceries.vue
@@ -20,26 +20,36 @@
         </v-expansion-panel-title>
         <v-expansion-panel-text>
 
+          <v-form ref="form">
 
           <v-row no-gutters>
             <v-spacer></v-spacer>
             <v-checkbox v-model="groceryItem.communal"> Communal</v-checkbox>
             <v-col>
-              <v-text-field v-model="groceryItem.name" placeholder="Item Name" ></v-text-field>
-              <v-text-field v-model="groceryItem.quantity" placeholder="Remaining" hide-details></v-text-field>
+              <v-text-field v-model="groceryItem.name"
+                placeholder="Item Name"
+                :rules="itemNameRules" ></v-text-field>
+              <v-text-field v-model="groceryItem.quantity" 
+                placeholder="Remaining" 
+                hide-details></v-text-field>
+                <v-spacer></v-spacer>
             </v-col>
             <v-col cols="5">
-              <v-select v-model="groceryItem.location" placeholder="location" :items="locations" chips flat variant="solo"></v-select>
+              <v-select v-model="groceryItem.location" label="Location" :items="locations"  ></v-select>
             </v-col>
             <v-divider vertical class="mx-4"></v-divider>
           </v-row>
+        
+
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn @click="saveGroceryItem()" variant="text" color="primary">
+            <v-btn @click="validateGroceryItem()" variant="text" color="primary">
               Save
             </v-btn>
+            
           </v-card-actions>
+        </v-form>
         </v-expansion-panel-text>
       </v-expansion-panel>
       <!-- End Panels-->
@@ -66,32 +76,50 @@ export default {
         household: this.$store.state.household
       },
       locations: ["freezer", "Cabinet1", "Cabinet2", "spice_rack"],
-      groceries: []
+      groceries: [],
+      itemNameRules: [
+        value => !!value || 'This field is required'
+      ]
       
     }
   },
   methods: {
-    // your methods here
+    validateGroceryItem(){
+      this.$refs.form.validate().then(data => {
+        if(data.valid === true){
+            this.saveGroceryItem()
+          } else {
+            window.alert("This field is required.")
+          }
+        } 
+      ).catch(error => {
+        console.log(error);
+        window.alert(error);
+      })
+    },
+
     async saveGroceryItem() {
+
       const url = 'http://localhost:5001/unpack-the-pantry-fc442/us-central1/saveItem/saveItem'
       const ownerID = "someID"//this.$store.state.user.displayName
       
-      const response = await axios.post(url, {
+      try {
+        const response = await axios.post(url, {
         ownerID: ownerID,
         groceryItem: this.groceryItem,
         household: this.household
-      }).then(response => {
-        if(response.status === 200){
-            window.alert('Item Saved Successfully');
-            //this.$store.commit('houseRegistered', houseHold)
-          }
-      }).catch(error => {
+      });
+
+      if(response.status === 200){
+        window.alert('Item Saved Successfully');
+        //this.$store.commit('houseRegistered', houseHold)
+      }
+      } catch (error) {
         console.error(error);
-        console.log("RESPOSNE: ");
-        console.log(response.data);
         window.alert('An error occurred while saving your Item.');
-      })
+      }
     }
+    
   },
   computed: {
     // your computed properties here
